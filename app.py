d9656fd1788dab2a41f3560a1f4657c1dd27f6a9
mfique <pacifiquem58@gmail.com>
2024-07-18 23:46:30 +0200

    Create and Read using python

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, flash
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # Required for flashing messages
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password, password):
+        return "Login successful!"  # Or redirect to a dashboard, etc.
+    else:
+        flash('Invalid credentials. Please try again.', 'error')
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful! You can now log in.', 'success')
+        return render_template('register.html')
+    
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
